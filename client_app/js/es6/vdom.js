83457cbca6dc8a4b7ba6c1d903d994b1742f470d
Justin Sullard <justin@justinsullard.com>
2017-05-25 00:52:19 -0500

    Performance tuning vdom
    Put in place an array flatten for element children, tightened up some other portions to make up the performance difference. May remove flatten at a later date but wanted to ensure it was available.

--- client_app/js/es6/vdom.js
+++ client_app/js/es6/vdom.js
@@ -1,51 +1,65 @@
-const El = (type, props = {}, ...children) => { return { type, props, children }; };
+const flatten = (arr) => {
+    const flat = [].concat(arr);
+    for (let i = 0; i < flat.length; i++) {
+        if (Array.isArray(flat[i])) {
+            flat.splice(i, 1, ...flat[i--]);
+        }
+    }
+    return flat;
+};
+
+const El = (type, props = {}, ...children) => ({ type, props, children: flatten(children) });
 
 const sBoolProp = (el, k, v) => {
     if (v) {
         el.setAttribute(k, v);
-        return el[k] = true;
+        el[k] = true;
+        return;
     }
     el[k] = false;
 };
 
 const dBoolProp = (el, k) => el[k] = false;
 
-const EV_REGEX = /^on/;
-const isEvProp = (k) => EV_REGEX.test(k);
-
-const getEv = (k) => k.slice(2).toLowerCase();
-
 const sProp = (el, k, v) => {
-    if (typeof v === 'boolean') { return sBoolProp(el, k, v); }
+    if (typeof v === 'boolean') {
+        sBoolProp(el, k, v);
+        return;
+    }
     el.setAttribute(k, v);
 };
 
 const dProp = (el, k, v) => {
     el.removeAttribute(k);
-    if (typeof v === 'boolean') { return dBoolProp(el, k); }
+    if (typeof v === 'boolean') { dBoolProp(el, k); }
 };
 
-const sProps = (el, p = {}) => Object.keys(p).forEach(k => sProp(el, k, p[k]));
+const sProps = (el, p = {}) => {
+    for (k in p) {
+        sProp(el, k, p[k]);
+    }
+};
 
 const uProp = (el, name, newVal, oldVal) => {
-    if (!newVal) { return dProp(el, name, oldVal); }
+    if (!newVal) {
+        dProp(el, name, oldVal);
+        return;
+    }
     if (!oldVal || newVal !== oldVal) { sProp(el, name, newVal); }
 };
 
 const uProps = (el, n, o = {}) => {
-    Object.keys(Object.assign({}, n, o)).forEach(k => {
+    const m = Object.assign({}, n, o)
+    for (k in m) {
         uProp(el, k, n[k], o[k]);
-    });
+    }
 };
-
-const addEventListener = (el, props = {}) => Object.keys(props).forEach(k => isEvProp(k) && el.addEventListener(getEv(k), props[k]));
 
 const createElementSVG = (doc, n) => {
     if (typeof n === 'string') { return doc.createTextNode(n); }
     const { props = {}, children = [] } = n;
     const el = doc.createElementNS('http://www.w3.org/2000/svg', n.type);
     sProps(el, props);
-    addEventListener(el, props);
     children.map(createElementSVG.bind(null, doc)).forEach(el.appendChild.bind(el));
     return el;
 };
@@ -57,7 +71,6 @@ const createElement = (doc, n) => {
     const { props = {}, children = [] } = n;
     const el = doc.createElement(type);
     sProps(el, props);
-    addEventListener(el, props);
     children.map(createElement.bind(null, doc)).forEach(el.appendChild.bind(el));
     return el;
 };
@@ -65,10 +78,19 @@ const createElement = (doc, n) => {
 const changed = (a, b) => typeof a !== typeof b || typeof a === 'string' && a !== b || a.type !== b.type;
 
 const renderSVG = (doc, p, n, o, i = 0) => {
-    if (!p) { return console.warn('No parent element provided to renderSVG') }
-    if (!o) { return p.appendChild(createElementSVG(n)); }
-    if (!n) { return p.removeChild(p.childNodes[i]); }
-    if (changed(n, o)) { return p.replaceChild(p.childNodes[i], createElementSVG(n)); }
+    if (!p) { throw new Error('No parent element provided to renderSVG'); }
+    if (!o) {
+        p.appendChild(createElementSVG(n));
+        return;
+    }
+    if (!n) {
+        p.removeChild(p.childNodes[i]);
+        return;
+    }
+    if (changed(n, o)) {
+        p.replaceChild(p.childNodes[i], createElementSVG(n));
+        return;
+    }
     if (n.type) {
         uProps(p.childNodes[i], n.props, o.props);
         for (let j = 0, l = Math.max(n.children.length, o.children.length); j < l; j ++) {
@@ -78,20 +100,33 @@ const renderSVG = (doc, p, n, o, i = 0) => {
 };
 
 const render = (doc, p, n, o, i = 0) => {
-    if (!p) { return console.warn('No parent element provided to render') }
-    if (!o) { return p.appendChild(createElement(doc, n)); }
-    if (!n) { return p.removeChild(p.childNodes[i]); }
-    if (changed(n, o)) { return p.replaceChild(p.childNodes[i], createElement(doc, n)); }
+    if (!p) { throw new Error('No parent element provided to render'); }
+    if (!o) {
+        p.appendChild(createElement(doc, n));
+        return;
+    }
+    if (!n) {
+        p.removeChild(p.childNodes[i]);
+        return;
+    }
+    if (changed(n, o)) {
+        p.replaceChild(p.childNodes[i], createElement(doc, n));
+        return;
+    }
     if (n.type) {
         if (n.type === 'svg') {
             renderSVG(doc, p, n, o, i);
         } else {
             uProps(p.childNodes[i], n.props, o.props);
-        for (let j = 0, l = Math.max(n.children.length, o.children.length); j < l; j ++) {
+            for (let j = 0, l = Math.max(n.children.length, o.children.length); j < l; j ++) {
                 render(doc, p.childNodes[i], n.children[j], o.children[j], j);
-            }            
+            }
         }
     }
 };
 
-module.exports = { render, El, uProps };
+module.exports = {
+    render,
+    El,
+    uProps
+};
